99f120cefc9f073dc0b0308b79739be228cc00fd
hazycora <hazycora@riseup.net>
2022-11-04 21:11:11 -0500

    add join page

---
+++ src/routes/join/+page.svelte
@@ -0,0 +1,38 @@
+<svelte:head>
+    <title>join besties</title>
+    
+    <meta name="title" content="join besties">
+    <meta name="description" content="apply to join the besties">
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://besties.house/">
+    <meta property="og:title" content="join besties">
+    <meta property="og:description" content="apply to join the besties">
+    <meta property="og:image" content="https://besties.house/besties-thumb.png">
+
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://besties.house/">
+    <meta property="twitter:title" content="join besties">
+    <meta property="twitter:description" content="apply to join the besties">
+    <meta property="twitter:image" content="https://besties.house/besties-thumb.png">
+</svelte:head>
+
+<div class="page">
+    <nav>
+        <div class="nav-inner">
+            <a href="/">
+                <p class="besties-heading">besties</p>
+            </a>
+        </div>
+    </nav>
+
+    <div class="section">
+        <div class="section-inner">
+            <h2>join the besties</h2>
+            <p>
+                no
+            </p>
+        </div>
+    </div>
+
+</div>
