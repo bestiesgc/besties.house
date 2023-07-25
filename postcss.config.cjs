5a89bf0deed2e0ca3c261ae732abca1316ba6645
hazycora <hazysu@riseup.net>
2023-07-25 14:22:13 -0500

    remove unused postcss plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,8 @@
 const autoprefixer = require('autoprefixer')
 const scrollbar = require('postcss-scrollbar')
-const apply = require('postcss-class-apply/dist/index')
 
 const config = {
-	plugins: [apply, scrollbar, autoprefixer]
+	plugins: [scrollbar, autoprefixer]
 }
 
 module.exports = config
